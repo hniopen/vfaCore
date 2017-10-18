1222544c10e718e30243062793147060c4dca936
safidy-rs <r.safydy@gmail.com>
2017-10-18 17:25:18 +0300

    add sample dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,14 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-10">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <section class="content-header">
+        <h1 class="pull-left">Dashboard</h1>
+    </section>
+    <div class="clearfix"></div>
+    <div class="content">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-yellow">
+                            <div class="inner">
+                                <h3>13</h3>
 
-                <div class="panel-body">
-                    You are logged in!
+                                <p>Registered Users</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-users"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">
+                                More info <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-green">
+                            <div class="inner">
+                                <h3>24</h3>
+
+                                <p>Existing KPIs</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-pie-chart"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">
+                                More info <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-red">
+                            <div class="inner">
+                                <h3>17</h3>
+
+                                <p>DW Questionnaire</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-clone"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">
+                                More info <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div><!-- ./col -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-aqua">
+                            <div class="inner">
+                                <h3>150.021</h3>
+
+                                <p>Pulled submissions</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-list"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">
+                                More info <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
                 </div>
+                <!-- /.row -->
             </div>
         </div>
     </div>
