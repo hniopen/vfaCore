c6564fdf1b754d2ae05e4bb3c86806e6df32a8ab
safidy-rs <r.safydy@gmail.com>
2018-02-21 15:48:56 +0300

    add seed for feature flag

---
+++ database/seeds/DefaultFeaturesFlagSeed.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use AlfredNutileInc\LaravelFeatureFlags;
+class DefaultFeaturesFlagSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Example
+//        $feature = new LaravelFeatureFlags\FeatureFlag();
+//        $feature->key = 'add-twitter-field';
+//        $feature->variants = [ 'users' => ['alfrednutile@gmail.com'] ];
+//        $feature->save();
+
+        $feature = new LaravelFeatureFlags\FeatureFlag();
+        $feature->key = 'feature_acl';//URR
+        $feature->variants = "on";
+        $feature->save();
+
+        $feature = new LaravelFeatureFlags\FeatureFlag();
+        $feature->key = 'feature_dw_sync';//dwsync package
+        $feature->variants = "on";
+        $feature->save();
+    }
+}
